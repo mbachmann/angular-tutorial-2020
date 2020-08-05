b7d9f03c220b3fc4d4ebfb2fee7b5aeeb0e21a71
bacn <mbachmann@trdi.io>
2020-08-05 18:11:25 +0200

    initial branch commit

--- src/app/auction-detail/auction-detail.component.ts
+++ src/app/auction-detail/auction-detail.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuctionDataService} from '../shared/auction-data.service';
 import {Subscription} from 'rxjs';
+import {Auction} from "../shared/auction";
 
 @Component({
   selector: 'app-auction-detail',
   templateUrl: './auction-detail.component.html',
   styleUrls: ['./auction-detail.component.scss']
 })
-export class AuctionDetailComponent implements OnInit {
+export class AuctionDetailComponent implements OnInit, OnDestroy {
 
+  auction: Auction;
   private routeSubscription: Subscription;
+  private subscription: Subscription;
 
   constructor(private auctionDataService: AuctionDataService,
               private route: ActivatedRoute,
@@ -21,6 +24,14 @@ export class AuctionDetailComponent implements OnInit {
   ngOnInit() {
     this.routeSubscription = this.route.params.subscribe(params => {
       console.log(params['id']);
+      this.subscription = this.auctionDataService.getHttpAuction(params['id']).subscribe(
+        auction => {
+          this.auction = auction;
+          // console.log('auction: ' + auction);
+          // this.adjustDate();
+        },
+        err => console.log(err)
+      );
 
     });
   }
@@ -30,5 +41,13 @@ export class AuctionDetailComponent implements OnInit {
     if (this.routeSubscription != null) {
       this.routeSubscription.unsubscribe();
     }
+
+    if (this.subscription != null)  {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  onContainerClick() {
+    this.router.navigate(['/auctions/' + this.auction.id]);
   }
 }
--- src/app/shared/auction-data.service.ts
+++ src/app/shared/auction-data.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Auction } from './auction';
-import { AUCTION_DATA } from './auction-data';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import {Auction} from './auction';
+import {AUCTION_DATA} from './auction-data';
+import {Observable, of} from 'rxjs';
+import {HttpClient} from "@angular/common/http";
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,7 @@ import { Observable, of } from 'rxjs';
 export class AuctionDataService {
 
   private auctions: Auction[] = AUCTION_DATA;
+  private URL='http://localhost:4730/auctions';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -17,12 +20,36 @@ export class AuctionDataService {
     return this.auctions;
   }
 
-  public getObservableAuctions(): Observable<Auction[]> {
+  getObservableAuctions(): Observable<Auction[]> {
     return of(this.auctions);
   }
 
   public getHttpAuctions(): Observable<Array<Auction>> {
-    return this.httpClient.get<Array<Auction>>('http://localhost:4730/auctions');
+    return this.httpClient.get<Array<Auction>>(this.URL);
+  }
+
+  public getHttpAuction(id: number): Observable<Auction> {
+    return this.httpClient.get<Auction>(this.URL + '/' + id);
+  }
+
+  public create(auction: Auction): Observable<Auction> {
+    return this.httpClient.post<Auction>(this.URL, auction);
+  }
+
+  public delete(auction: Auction): Observable<Auction> {
+    return this.httpClient.delete<Auction>(`${this.URL}/${auction.id}`);
+  }
+
+  public get(id: number): Observable<Auction> {
+    return this.httpClient.get<Auction>(this.URL + '/' + id);
+  }
+
+  public list(): Observable<Array<Auction>> {
+    return this.httpClient.get<Array<Auction>>(this.URL);
+  }
+
+  public update(auction: Auction): Observable<Auction> {
+    return this.httpClient.put<Auction>(`${this.URL}/${auction.id}`, auction);
   }
 
 }
