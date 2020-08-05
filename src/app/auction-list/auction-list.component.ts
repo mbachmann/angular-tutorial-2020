1542f88a9bff172438e7152a6669b728aff64a02
bacn <mbachmann@trdi.io>
2020-08-05 15:51:19 +0200

    initial branch commit

--- src/app/auction-list/auction-list.component.ts
+++ src/app/auction-list/auction-list.component.ts
@@ -4,12 +4,13 @@ import {AuctionDataService} from '../shared/auction-data.service';
 import {Observable} from 'rxjs';
 import {Subscription} from 'rxjs';
 
+
 @Component({
   selector: 'app-auction-list',
   templateUrl: './auction-list.component.html',
   styleUrls: ['./auction-list.component.scss']
 })
-export class AuctionListComponent implements OnInit {
+export class AuctionListComponent implements OnInit, OnDestroy {
   @Input() headerTitle: string;
   @Output() titleClicked = new EventEmitter<string>();
 
@@ -19,15 +20,19 @@ export class AuctionListComponent implements OnInit {
 
   constructor(private auctionDataService: AuctionDataService) {
     // this.auctions = auctionDataService.getAuctions();
-    this.auctionsObservable = auctionDataService.getObservableAuctions();
-    this.auctionSubscription = this.auctionsObservable.subscribe(data => this.auctions = data);
   }
 
   ngOnInit() {
+    this.auctionsObservable = this.auctionDataService.getObservableAuctions();
+    this.auctionSubscription = this.auctionsObservable.subscribe(data => this.auctions = data);
   }
 
   onTitleClicked(event: MouseEvent): void {
     this.titleClicked.emit('Title clicked' + event.type);
+  }
+
+  ngOnDestroy() {
+    this.auctionSubscription.unsubscribe();
   }
 
 }
