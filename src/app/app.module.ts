8713817e5b6ac69fa5dafa185b79ce461f7faaa4
bacn <mbachmann@trdi.io>
2020-08-05 10:56:36 +0200

    initial branch commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuctionListComponent } from './auction-list/auction-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuctionListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/auction-list/auction-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auction-list',
+  templateUrl: './auction-list.component.html',
+  styleUrls: ['./auction-list.component.scss']
+})
+export class AuctionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
