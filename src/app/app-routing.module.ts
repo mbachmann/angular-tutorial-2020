1acd1a11c4e4fdaf7dbe4614640ad2b1c38456e1
bacn <mbachmann@trdi.io>
2020-08-05 17:27:45 +0200

    initial branch commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuctionListComponent} from './auction-list/auction-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
