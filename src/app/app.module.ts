8b5c43af4f3c9700acb967a878d55ba4301b6cdc
bacn <mbachmann@trdi.io>
2020-08-05 11:03:30 +0200

    initial branch commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuctionListComponent } from './auction-list/auction-list.component';
+import { MouseEventDisplayComponent } from './mouse-event-display/mouse-event-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuctionListComponent
+    AuctionListComponent,
+    MouseEventDisplayComponent
   ],
   imports: [
     BrowserModule,
