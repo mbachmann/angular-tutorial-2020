555c303a419a0b8c289e27222b1fa08dd29daa7c
bacn <mbachmann@trdi.io>
2020-09-04 12:26:38 +0200

    add the environment api to the auction service. Add the mock backend provider to the spec.

--- src/app/auction/shared/auction-data.service.spec.ts
+++ src/app/auction/shared/auction-data.service.spec.ts
@@ -1,24 +1,56 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AuctionDataService } from './auction-data.service';
+import {AuctionDataService} from './auction-data.service';
 import {RouterTestingModule} from '@angular/router/testing';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {MockBackendInterceptor} from '../../shared/helper/mock/mock-backend-interceptor.service';
 
 describe('AuctionDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      RouterTestingModule,
-      FormsModule,
-      ReactiveFormsModule,
-      HttpClientTestingModule
-    ],
+  let service: AuctionDataService;
 
-    providers: [AuctionDataService]
-  }));
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([
+          {path: 'login', redirectTo: ''}]),
+        HttpClientTestingModule,
+
+      ],
+      providers: [
+        AuctionDataService,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: MockBackendInterceptor,
+          multi: true
+        }
+      ]
+    });
+    service = TestBed.inject(AuctionDataService);
+
+  });
+
 
   it('should be created', () => {
-    const service: AuctionDataService = TestBed.inject(AuctionDataService);
     expect(service).toBeTruthy();
   });
+
+  it('should load an auction', (done) => {
+    service.getHttpAuction(1).subscribe(
+      (data) => {
+        console.log('auctionData=' + data.id);
+        expect(data.id).toBe(1);
+        done();
+      },
+      (err) => {
+        console.log('auctionResponse', err);
+        fail();
+        done();
+      }, () => {
+        // console.log('Test complete');
+        done();
+      });
+  });
+
 });
--- src/app/auction/shared/auction-data.service.ts
+++ src/app/auction/shared/auction-data.service.ts
@@ -3,6 +3,7 @@ import {Auction} from './auction';
 import {AUCTION_DATA} from './auction-data';
 import {Observable, of} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
 
 
 
@@ -12,9 +13,11 @@ import {HttpClient} from '@angular/common/http';
 export class AuctionDataService {
 
   private auctions: Auction[] = AUCTION_DATA;
-  private URL = 'http://localhost:4730/auctions';
+  private backendApiUrl;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+    this.backendApiUrl = environment.endpoints.backendAuthUrl;
+  }
 
   public getAuctions(): Auction[] {
     return this.auctions;
@@ -25,31 +28,31 @@ export class AuctionDataService {
   }
 
   public getHttpAuctions(): Observable<Array<Auction>> {
-    return this.httpClient.get<Array<Auction>>(this.URL);
+    return this.httpClient.get<Array<Auction>>(this.backendApiUrl + '/auctions');
   }
 
   public getHttpAuction(id: number): Observable<Auction> {
-    return this.httpClient.get<Auction>(this.URL + '/' + id);
+    return this.httpClient.get<Auction>(this.backendApiUrl + '/auctions/' + id);
   }
 
   public create(auction: Auction): Observable<Auction> {
-    return this.httpClient.post<Auction>(this.URL, auction);
+    return this.httpClient.post<Auction>(this.backendApiUrl + '/auctions', auction);
   }
 
   public delete(auction: Auction): Observable<Auction> {
-    return this.httpClient.delete<Auction>(`${this.URL}/${auction.id}`);
+    return this.httpClient.delete<Auction>(`${this.backendApiUrl}/auctions/${auction.id}`);
   }
 
   public get(id: number): Observable<Auction> {
-    return this.httpClient.get<Auction>(this.URL + '/' + id);
+    return this.httpClient.get<Auction>(this.backendApiUrl + '/auctions/' + id);
   }
 
   public list(): Observable<Array<Auction>> {
-    return this.httpClient.get<Array<Auction>>(this.URL);
+    return this.httpClient.get<Array<Auction>>(this.backendApiUrl + '/auctions');
   }
 
   public update(auction: Auction): Observable<Auction> {
-    return this.httpClient.put<Auction>(`${this.URL}/${auction.id}`, auction);
+    return this.httpClient.put<Auction>(`${this.backendApiUrl}/auctions/${auction.id}`, auction);
   }
 
 }
